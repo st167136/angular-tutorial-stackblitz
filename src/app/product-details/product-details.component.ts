e7f568899206350790e10d7fa88bb49ff20c1811
st167136 <83642871+st167136@users.noreply.github.com>
2022-10-04 17:41:36 +0200

    5 files modified, 10 added (bin @create a shipping component)

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   product: Product | undefined;
 
@@ -17,7 +20,13 @@ export class ProductDetailsComponent implements OnInit {
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
 
-    this.product = products.find(product => product.id === productIdFromRoute)
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
   }
 
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Added to cart!');
+  }
 }
